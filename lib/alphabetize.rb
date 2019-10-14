ab0345710d42522f1496d38605fce774fc338b4c
Mei Wong <fongmeiw@gmail.com>
2019-10-14 18:32:11 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,10 @@
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   
   arr.sort_by do |word|
     word.split('').collect do |letter|
-      esperanto_alphabet.index(letter)
+      ALPHABET.index(letter)
     end
   end
 end
